b2e89073af5f2a674d514fc8d4c962d94f9e8a7b
elir-mahad <mmahad280@gmail.com>
2022-01-12 10:31:04 -0500

    Add config code for the dependency in next.config and add png image optimization dependency which is required for the next-optimized-images dependency

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@
 // (But you could use ES2015 features supported by your Node.js version)
 
 const debug = process.env.NODE_ENV !== "production";
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
 module.exports = {
   assetPrefix: !debug ? "/notes/" : "",
 };
+
+module.exports = withPlugins([optimizedImages]);
