2fad61c6425509bb4bc7a971c1df5b4554f38ce3
elir-mahad <mmahad280@gmail.com>
2022-01-13 11:03:49 -0500

    Add all jsx to featured component. Clean up home.module.css

--- components/Featured.js
+++ components/Featured.js
@@ -1,13 +1,25 @@
-// import CodingWithOthers from "../pages/coding-with-others";
 import style from "../styles/Home.module.css";
 //
 function Featured() {
   return (
     <div>
-      {/* <CodingWithOthers /> */}
-      <div className={style.description}>
-        <p className={style.oneLiner}>
-          I write about
+      <div style={{ border: "1px solid black", padding: "1rem" }}>
+        <p>Name : Mahad Mohamood</p>
+        <p>Title: Front-end developer</p>
+        <p>
+          Space: This is my personal blog. I write about things that i learn,
+          things that i want to remember, and things that i want to teach.
+        </p>
+        <p>Focus: </p>
+        <p>
+          “Today’s process of transition allows us to perceive what we are
+          losing and what we are gaining- this perception will become impossible
+          the moment we fully embrace, and feel fully at home in, the new
+          technologies. In short, we have the privilege of occupying the place
+          of ‘vanishing mediators’.”[plague of fantasies, zizek, pg.166]
+        </p>
+        <div className={style.description}>
+          <p className={style.oneLiner}>I write about</p>
           <div className={style.words}>
             <span className={style.word}>react</span>
             <span className={style.word}>javascript</span>
@@ -15,7 +27,7 @@ function Featured() {
             <span className={style.word}>data</span>
             <span className={style.word}>drawing</span>
           </div>
-        </p>
+        </div>
       </div>
     </div>
   );
