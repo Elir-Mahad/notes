28b125dcf723bd453bc2eb36d326e9f5e20df1b5
elir-mahad <mmahad280@gmail.com>
2022-01-13 09:06:36 -0500

    Add :nth-child selector animation for the landing page. Resource link in the comments of this commit

--- components/Featured.js
+++ components/Featured.js
@@ -1,9 +1,22 @@
-import CodingWithOthers from "../pages/coding-with-others";
-
+// import CodingWithOthers from "../pages/coding-with-others";
+import style from "../styles/Home.module.css";
+//
 function Featured() {
   return (
     <div>
-      <CodingWithOthers />
+      {/* <CodingWithOthers /> */}
+      <div className={style.description}>
+        <p className={style.oneLiner}>
+          I write about
+          <div className={style.words}>
+            <span className={style.word}>react</span>
+            <span className={style.word}>javascript</span>
+            <span className={style.word}>python</span>
+            <span className={style.word}>data</span>
+            <span className={style.word}>drawing</span>
+          </div>
+        </p>
+      </div>
     </div>
   );
 }
