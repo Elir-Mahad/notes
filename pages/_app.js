07802bf4cc1c54df765d53e98e528fda95b6c41c
elir-mahad <mmahad280@gmail.com>
2022-01-15 07:49:46 -0500

    In app.js, add imports necessary for google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 //
+import { useEffect } from "react";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+//
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
