1f0679be27587058b1829ad47533142d04114ec7
elir-mahad <mmahad280@gmail.com>
2022-01-18 07:06:12 -0500

    Remove commented out code in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
-//
-// import { useEffect } from "react";
 import Script from "next/script";
-// import { useRouter } from "next/router";
-// import * as gtag from "../lib/gtag";
 //
 function MyApp({ Component, pageProps }) {
   //
