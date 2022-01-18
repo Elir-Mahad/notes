69ae6957b0c56b953112ef7d643e49c8dea2ad5a
elir-mahad <mmahad280@gmail.com>
2022-01-18 06:57:55 -0500

    Relocating GA code to env caused error. Fixed error. Details in comment of commit.

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,9 @@ function MyApp({ Component, pageProps }) {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
         `}
       </Script>
       <Component {...pageProps} />
