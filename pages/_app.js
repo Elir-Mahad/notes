8dbb9c6065b80601529debb4c0bf1340446155d3
elir-mahad <mmahad280@gmail.com>
2022-01-18 06:36:53 -0500

    Insert google analytics in local env

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
     <Layout>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-N5QH1XFTC1"`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
       <Script strategy="lazyOnload">
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-N5QH1XFTC1');
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
         `}
       </Script>
       <Component {...pageProps} />
