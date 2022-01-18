c1ef2777c505fa73f09c4343efbc5c8d7f5e6c85
elir-mahad <mmahad280@gmail.com>
2022-01-18 12:48:14 -0500

    Add imports to TopicsMenu

--- components/TopicsMenu.js
+++ components/TopicsMenu.js
@@ -1,5 +1,17 @@
-import React from "react";
-
+import React, { useState } from "react";
+import {
+  BlogName,
+  BlogsMenu,
+  TopBlogsMenu,
+  BlogMenuBox,
+  TbMheading,
+  TbMicon,
+  CustomMenu,
+} from "../styles/Styles.js";
+import Link from "next/link";
+import { MdClose } from "react-icons/md";
+import { GrMenu } from "react-icons/gr";
+//
 function TopicsMenu() {
   return (
     <div>
