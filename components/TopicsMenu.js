47a364f0c0185c65bfacd6b9a9f4ab5e43b7610e
elir-mahad <mmahad280@gmail.com>
2022-01-18 12:50:51 -0500

    Add tagged template literals to the return statement of TopicsMenu

--- components/TopicsMenu.js
+++ components/TopicsMenu.js
@@ -32,9 +32,100 @@ function TopicsMenu() {
     setCloseIcon(false);
   };
   return (
-    <div>
-      <h1>Topics menu</h1>
-    </div>
+    <CustomMenu>
+      <BlogMenuBox>
+        <TopBlogsMenu>
+          {openIcon && (
+            <TbMicon onClick={showContent}>
+              <GrMenu />
+            </TbMicon>
+          )}
+          {closeIcon && (
+            <TbMicon onClick={hideContent}>
+              <MdClose />
+            </TbMicon>
+          )}
+          <TbMheading>Mahad's notes</TbMheading>
+        </TopBlogsMenu>
+
+        {menuContent && (
+          <BlogsMenu>
+            {/*  */}
+            <Link
+              //
+              href="/"
+              as={process.env.BACKEND_URL + "/"}
+            >
+              <BlogName onClick={hideContent}>About me</BlogName>
+            </Link>
+            {/*  */}
+            <Link href="https://mahad.codes/">
+              <BlogName onClick={hideContent}>Portfolio</BlogName>
+            </Link>
+            {/*  */}
+            <Link
+              //
+              href="/add-remote-url-to-local-repo"
+              as={process.env.BACKEND_URL + "/add-remote-url-to-local-repo"}
+            >
+              <BlogName onClick={hideContent}>
+                How to add a remote url to your local repository
+              </BlogName>
+            </Link>
+            {/*  */}
+            <Link
+              //
+              href="/coding-with-others"
+              as={process.env.BACKEND_URL + "/coding-with-others"}
+            >
+              <BlogName onClick={hideContent}>
+                How to code with other people
+              </BlogName>
+            </Link>
+            {/*  */}
+            <Link
+              //
+              href="/connect-github-account-to-computer"
+              as={
+                process.env.BACKEND_URL + "/connect-github-account-to-computer"
+              }
+            >
+              <BlogName onClick={hideContent}>
+                How to connect your github account to your computer
+              </BlogName>
+            </Link>
+            {/*  */}
+            <Link
+              //
+              href="/deploy-netlify"
+              as={process.env.BACKEND_URL + "/deploy-netlify"}
+            >
+              <BlogName onClick={hideContent}>
+                How to deploy a site on Netlify.
+              </BlogName>
+            </Link>
+            {/*  */}
+            <Link
+              //
+              href="/deploy-react-github-pages"
+              as={process.env.BACKEND_URL + "/deploy-react-github-pages"}
+            >
+              <BlogName onClick={hideContent}>
+                How to deploy a react site on github pages.
+              </BlogName>
+            </Link>
+            {/*  */}
+            <Link
+              //
+              href="/writing-commits"
+              as={process.env.BACKEND_URL + "/writing-commits"}
+            >
+              <BlogName onClick={hideContent}>How to write commits</BlogName>
+            </Link>
+          </BlogsMenu>
+        )}
+      </BlogMenuBox>
+    </CustomMenu>
   );
 }
 
