0e6e9c165d09c13a21d6fdb5fa0f3fd787fd92de
elir-mahad <mmahad280@gmail.com>
2022-02-07 17:28:29 -0500

    Testing 2 GH-17

--- pages/writing-commits.js
+++ pages/writing-commits.js
@@ -25,7 +25,7 @@ function WritingCommits() {
       </Head>
       <PostTitle className={style.weight100}>
         How to write commits properly
-        {/* Testing */}
+        {/* Testing 2*/}
       </PostTitle>
       <BlogBody>
         <Sentence>
