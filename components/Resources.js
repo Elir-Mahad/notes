f5d1daf888404ebcbbc606abf2fe3c83e2444eb2
elir-mahad <mmahad280@gmail.com>
2022-02-07 11:19:24 -0500

    Create resources componen in components folder

---
+++ components/Resources.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Resources() {
+  return (
+    <div>
+      <p>Learn more here:</p>
+      <a>First url</a>
+      <a>Second url</a>
+      <a>Third url</a>
+    </div>
+  );
+}
+
+export default Resources;
