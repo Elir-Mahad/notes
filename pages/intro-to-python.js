6ae3daeb4b91a69dbcb32ab40af4deadba89e285
elir-mahad <mmahad280@gmail.com>
2022-02-08 17:42:24 -0500

    content(intropython): add tagged template literals to component

--- pages/intro-to-python.js
+++ pages/intro-to-python.js
@@ -1,9 +1,36 @@
-import React from "react";
+import Head from "next/head";
+import {
+  //
+  BlogBody,
+  PostTitle,
+} from "../styles/Styles.js";
+// import Resources from "../components/Resources";
+import style from "../styles/Home.module.css";
 
 function IntroPython() {
   return (
     <div>
-      <h1>Intro to python</h1>
+      <Head>
+        <title>Intro to python</title>
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <PostTitle className={style.weight100}>Intro to python</PostTitle>
+      <BlogBody></BlogBody>
+      {/* <Resources
+        //
+        topic="intro to python"
+        firstResTitle=""
+        firstLink=""
+        secondResTitle=""
+        secondLink=""
+        thirdResTitle=""
+        thirdLink=""
+      /> */}
     </div>
   );
 }
