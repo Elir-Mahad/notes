dbc9ba43473bb65ebe514ed8b0e1c5b76fea00ab
elir-mahad <mmahad280@gmail.com>
2022-02-09 10:42:21 -0500

    content(PostMenu): add all menu options

--- components/PostMenu.js
+++ components/PostMenu.js
@@ -4,6 +4,21 @@ function PostMenu() {
   return (
     <div>
       <h1>Post menu</h1>
+      <p>Overview of numpy package </p>
+      <p>Convert a list into a numpy array</p>
+      <p>Print out the mean of a numpy array</p>
+      <p>Print out the median of a numpy array</p>
+      <p>Overview of 2d numpy arrays</p>
+      <p>Print out the shape of a 2dnumpy array</p>
+      <p>Print out a specific index of a 2dnumpy array</p>
+      <p>Print out columns of a 2dnumpy array</p>
+      <p>Print out a row of a 2dnumpy array</p>
+      <p>Print out a column of a 2dnumpy array</p>
+      <p>Print out the mean of a 2d numpy array</p>
+      <p>Print out the median of a 2d numpy array</p>
+      <p>
+        Print out out the ‘correlation coefficient matrix’ of a 2d numpy array
+      </p>
     </div>
   );
 }
