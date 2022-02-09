8061e68ca145d2d7257c6818eedac8286d6ad7f8
elir-mahad <mmahad280@gmail.com>
2022-02-09 06:26:31 -0500

    feat(intrpychaps): create introPyTwo component

---
+++ components/intropychaps/IntroPyTwo.js
@@ -0,0 +1,18 @@
+import {
+  //
+  Chapter,
+  Code,
+  Litem,
+  Sentence,
+  SubHeading,
+} from "../../styles/Styles.js";
+
+function IntroPyTwo() {
+  return (
+    <div>
+      <h1>second chap</h1>
+    </div>
+  );
+}
+
+export default IntroPyOne;
