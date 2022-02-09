863ce40996e77b7f853f31aceccfd863f12a47f2
elir-mahad <mmahad280@gmail.com>
2022-02-09 07:13:46 -0500

    feat(intropychaps): create IntroPyFour component

---
+++ components/intropychaps/IntroPyFour.js
@@ -0,0 +1,18 @@
+import {
+  //
+  Chapter,
+  Code,
+  Litem,
+  Sentence,
+  SubHeading,
+} from "../../styles/Styles.js";
+
+function IntroPyFour() {
+  return (
+    <div>
+      <Chapter> Chapter 3</Chapter>
+    </div>
+  );
+}
+
+export default IntroPyFour;
