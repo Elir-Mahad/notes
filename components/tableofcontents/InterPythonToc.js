303edb596bb8e16703bd3df53cb1ab12d4c5f166
elir-mahad <mmahad280@gmail.com>
2022-02-10 18:37:48 -0500

    feat(tableofcontents): create interpythontoc

---
+++ components/tableofcontents/InterPythonToc.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function InterPythonToc() {
+  return (
+    <div>
+      <h1>Inter python table of contents</h1>
+    </div>
+  );
+}
+
+export default InterPythonToc;
