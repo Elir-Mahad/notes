047a3004f5f392982e97cfddd24c48526f90b6c7
elir-mahad <mmahad280@gmail.com>
2022-02-10 18:58:55 -0500

    enhance(interpythontoc): add react scroll links and ids

--- components/tableofcontents/InterPythonToc.js
+++ components/tableofcontents/InterPythonToc.js
@@ -9,7 +9,7 @@ import {
 import style from "../../styles/Home.module.css";
 import { CgMenuGridO } from "react-icons/cg";
 import { CgMenuMotion } from "react-icons/cg";
-// import { Link } from "react-scroll";
+import { Link } from "react-scroll";
 //
 function InterPythonToc() {
   //
@@ -46,35 +46,42 @@ function InterPythonToc() {
         <PmOptions onMouseLeave={hideContent}>
           <PmOtitle>Table of contents</PmOtitle>
           <ol style={{ listStylePosition: "inside", padding: "0rem" }}>
-            {/* Chapter one below 3 parts */}
-
-            <PmOption onClick={hideContent} className={style.pmitem}>
-              C1: How to make a plot with two lists.
-            </PmOption>
-
-            <PmOption onClick={hideContent} className={style.pmitem}>
-              C1: How to make a scatter plot with two lists.
-            </PmOption>
-
-            <PmOption onClick={hideContent} className={style.pmitem}>
-              C1: What are histograms ?
-            </PmOption>
-
-            <PmOption onClick={hideContent} className={style.pmitem}>
-              C1: How to make a histogram ?
-            </PmOption>
-
-            <PmOption onClick={hideContent} className={style.pmitem}>
-              C1: Population pyramid with age, gender, and population.
-            </PmOption>
-
-            <PmOption onClick={hideContent} className={style.pmitem}>
-              C1: How to add axis labels and plot title.
-            </PmOption>
-
-            <PmOption onClick={hideContent} className={style.pmitem}>
-              C1: How to add ticks (custom axis values) and historical data.
-            </PmOption>
+            {/* Chapter one below 7 parts */}
+            <Link to="chap1.1" spy={true} smooth={true}>
+              <PmOption onClick={hideContent} className={style.pmitem}>
+                C1: How to make a plot with two lists.
+              </PmOption>
+            </Link>
+            <Link to="chap1.2" spy={true} smooth={true}>
+              <PmOption onClick={hideContent} className={style.pmitem}>
+                C1: How to make a scatter plot with two lists.
+              </PmOption>
+            </Link>
+            <Link to="chap1.3" spy={true} smooth={true}>
+              <PmOption onClick={hideContent} className={style.pmitem}>
+                C1: What are histograms ?
+              </PmOption>
+            </Link>
+            <Link to="chap1.4" spy={true} smooth={true}>
+              <PmOption onClick={hideContent} className={style.pmitem}>
+                C1: How to make a histogram ?
+              </PmOption>
+            </Link>
+            <Link to="chap1.5" spy={true} smooth={true}>
+              <PmOption onClick={hideContent} className={style.pmitem}>
+                C1: Population pyramid with age, gender, and population.
+              </PmOption>
+            </Link>
+            <Link to="chap1.6" spy={true} smooth={true}>
+              <PmOption onClick={hideContent} className={style.pmitem}>
+                C1: How to add axis labels and plot title.
+              </PmOption>
+            </Link>
+            <Link to="chap1.7" spy={true} smooth={true}>
+              <PmOption onClick={hideContent} className={style.pmitem}>
+                C1: How to add ticks (custom axis values) and historical data.
+              </PmOption>
+            </Link>
           </ol>
         </PmOptions>
       )}
