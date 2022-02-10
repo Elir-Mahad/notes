cc54fbe7f70dad6436628d51bfa4243a92c76c3b
elir-mahad <mmahad280@gmail.com>
2022-02-10 16:23:32 -0500

    content(interpyone): add some notes, chore(assets): add interpy graphs #2

--- components/interpychaps/InterPyOne.js
+++ components/interpychaps/InterPyOne.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { SubHeading, Code } from "../../styles/Styles";
 
 function InterPyOne() {
   return (
     <div>
-      <h1>Inter py chap one</h1>
+      <SubHeading>How to make a plot with two lists. </SubHeading>
+
+      <Code>
+        import matplotlib.pyplot as plt <br />
+        # import matplotlib <br />
+        year = [1950, 1970, 1990, 2010] <br />
+        # the variable year contains a list of years <br />
+        pop = [2.519, 3.692, 5.263, 6.972] <br />
+        # the variable pop contains a list of populations <br />
+        plt.plot(year, pop) <br />
+        # make a plot <br />
+        # x-axis values = year, y-axis values= population <br />
+        plt.show() <br /># show the plot
+      </Code>
     </div>
   );
 }
