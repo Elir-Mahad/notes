2a838c058b3a4b318595371dd01d46dfc0ab05ef
elir-mahad <mmahad280@gmail.com>
2022-02-10 15:58:54 -0500

    feat(pages): create intermediate-python page

---
+++ pages/intermediate-python.js
@@ -0,0 +1,50 @@
+import Head from "next/head";
+import {
+  //
+  BlogBody,
+  PostTitle,
+  Sentence,
+} from "../styles/Styles.js";
+// import Resources from "../components/Resources";
+import style from "../styles/Home.module.css";
+//
+function InterPython() {
+  return (
+    <div>
+      <Head>
+        <title>Intermediate python</title>
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <PostTitle className={style.weight100}>Intermediate python</PostTitle>
+      {/* <PostMenu /> */}
+      <BlogBody>
+        <Sentence>
+          {" "}
+          In this post, i will record some of my notes from datacamps
+          intermediate python course. I am big fan of javascript, and i noticed
+          that there are a lot of similarities between javascript and python.
+          Therefore, wherever I see similarities between javascript and python,
+          i will point them out.{" "}
+        </Sentence>
+        {/*  */}
+      </BlogBody>
+      {/* <Resources
+        //
+        topic="intro to python"
+        firstResTitle=""
+        firstLink=""
+        secondResTitle=""
+        secondLink=""
+        thirdResTitle=""
+        thirdLink=""
+      /> */}
+    </div>
+  );
+}
+
+export default InterPython;
