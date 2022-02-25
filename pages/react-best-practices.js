650b4be6714e84ab51a9f1af6d70867842559470
elir-mahad <mmahad280@gmail.com>
2022-02-25 16:56:49 -0500

    enhance(reactbp): add meta tags

--- pages/react-best-practices.js
+++ pages/react-best-practices.js
@@ -1,1 +1,19 @@
-// new page
+import Head from "next/head";
+//
+function ReactBp() {
+  return (
+    <div>
+      <Head>
+        <title>React best practices</title>
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+    </div>
+  );
+}
+
+export default ReactBp;
