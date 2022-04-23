29b6f73f60f1ba153bd01a519eb9f2251455af0c
elir-mahad <mmahad280@gmail.com>
2022-04-23 11:00:31 -0400

    enhance(src): add hooks folder with toogle hook

---
+++ hooks/useToggle.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const useToggle = (initialState) => {
+  const [isToggled, setIsToggled] = React.useState(initialState);
+  const toggle = React.useCallback(
+    () => setIsToggled((state) => !state),
+    [setIsToggled]
+  );
+  return [isToggled, toggle];
+};
