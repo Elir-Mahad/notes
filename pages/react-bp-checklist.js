c53a2271e90fee7984d9722bf5f76af525115f7b
elir-mahad <mmahad280@gmail.com>
2022-04-23 14:43:42 -0400

    enhance(reactchecklist): add keywords and description meta tag contents

--- pages/react-bp-checklist.js
+++ pages/react-bp-checklist.js
@@ -14,8 +14,14 @@ function ReactBpCheckList() {
     <div>
       <Head>
         <title>React best practices</title>
-        <meta name="keywords" content="" />
-        <meta name="description" content="" />
+        <meta
+          name="keywords"
+          content="react best practices, react.js, react, props, naming, jsx, javascript, javascript frameworks"
+        />
+        <meta
+          name="description"
+          content="In this tutorial, you will learn about react best practices. Each rule will contain code snippets showing bad code vs good code."
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400&display=swap"
           rel="stylesheet"
